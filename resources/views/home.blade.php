c300d6794903aabf0e760e66b0b05407805dae95
Renato de Azevedo Lacerda <renatoads1@gmail.com>
2019-04-30 17:29:12 -0300

    primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+        <p>aqui vc mete cod f...</p>
+@stop
